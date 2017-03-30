762ce8b8994c77a38a9f88b86f94e32889bba5b9
mingge <uu993@163.com>
2017-03-30 17:59:32 +0800

    ping++测试

---
+++ example/src/main/java/com/leaf/Magic/wxapi/WXPayEntryActivity.java
@@ -0,0 +1,30 @@
+package com.leaf.Magic.wxapi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.leaf.Magic.R;
+
+public class WXPayEntryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wxpay_entry2);
+        initView();
+    }
+
+    private void initView() {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
